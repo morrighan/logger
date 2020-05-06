0388c5d652647ff2bb02fc8f61adfa2b2b15c877
Yongbin Min <contact@cichol.com>
2020-05-06 18:00:29 +0900

    Prepare to release `v2.0.0-beta.0`

---
+++ sources/helpers/logging-level.ts
@@ -0,0 +1,22 @@
+enum LoggingLevel {
+    // Critical levels. (It will crash the process)
+    Fatal = 0,
+    Error = 1,
+
+    // Warning levels. (It just report warning information, not crash the process)
+    Warning = 2,
+
+    // Regular level and aliases.
+    Notice = 3,
+    Info = 3,
+
+    // Maybe sensitive levels. (In general, does not useful)
+    Http = 4,
+    Query = 4,
+
+    // Loud levels.
+    Verbose = 5,
+    Debug = 5
+}
+
+export default LoggingLevel;
